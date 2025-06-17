721314c667590088f2b09807ff72306610a8cf40
harlock024 <hadith024@gmail.com>
2025-06-16 20:02:46 -0600

    feat: Switch to Axios for API requests and centralize client logic

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -5,8 +5,6 @@ import { persist } from "zustand/middleware";
 
 type AuthState = {
   user: User | null;
-
-  refreshToken: string | null;
   isLoading: boolean;
   register: (
     username: string,
@@ -15,26 +13,18 @@ type AuthState = {
   ) => Promise<void>;
   login: (email: string, password: string) => Promise<void>;
   logout: () => void;
-  refresh: () => Promise<void>;
 };
 
 export const useAuthStore = create<AuthState>()(
   persist(
     (set, get) => ({
       user: null,
-      accessToken: null,
-      refreshToken: null,
       isLoading: false,
       register: async (username: string, email: string, password: string) => {
         try {
-          const { user, refresh_token } = await register(
-            username,
-            email,
-            password,
-          );
+          const { user } = await register(username, email, password);
           set({
             user,
-            refreshToken: refresh_token,
           });
         } catch (error) {
           console.error("Registration failed:", error);
@@ -42,30 +32,17 @@ export const useAuthStore = create<AuthState>()(
       },
       login: async (email: string, password: string) => {
         try {
-          const { user, refresh_token } = await login(email, password);
+          const { user } = await login(email, password);
           set({
             user,
-
-            refreshToken: refresh_token,
           });
         } catch (error) {
           console.error("Login failed:", error);
         }
       },
       logout: () => {
-        set({ user: null, refreshToken: null });
+        set({ user: null });
         localStorage.clear();
-      },
-      refresh: async () => {
-        const { refreshToken } = get();
-        if (!refreshToken) return;
-        try {
-          // const { accessToken } = await res.json();
-          // set({ accessToken });
-        } catch (error) {
-          console.error(error);
-          get().logout();
-        }
       },
     }),
     {
