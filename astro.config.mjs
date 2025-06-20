ad26ed3feed417db9f65683021c49bfc624a4680
harlock024 <hadith024@gmail.com>
2025-06-20 16:38:02 -0600

    Switch to Vercel adapter for Astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { API_URL } from "./src/services/api_url";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "middleware",
-  }),
   output: "server",
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
