3a5df718cdc45b1ea03340a717225d37435b417d
harlock024 <hadith024@gmail.com>
2026-07-14 17:02:18 -0600

    feat: node adapter changer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,28 @@ import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import { API_URL } from "./src/services/api_url";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-import vercel from "@astrojs/vercel";
-
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: parseInt(process.env.BUN_PORT ?? "4321"),
-  },
-  output: "server",
-  integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  adapter: vercel(),
-  env: {
-    schema: {
-      PUBLIC_BEEL_API: envField.string({ context: "client", access: "public", optional: true }),
-      BUN_PORT: envField.number({ context: "server", access: "public", default: 4321}),
-    },
-  },
+adapter: node({
+	mode: "standalone",
+}),
+server: {
+	port: parseInt(process.env.BUN_PORT ?? "4321"),
+},
+output: "server",
+integrations: [react()],
+vite: {
+	plugins: [tailwindcss()],
+},
+env: {
+	schema: {
+		PUBLIC_BEEL_API: envField.string({ context: "client", access: "public", optional: true }),
+		BUN_PORT: envField.number({ context: "server", access: "public", default: 4321}),
+	},
+},
 });
